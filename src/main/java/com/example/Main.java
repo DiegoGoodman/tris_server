33f97ba8164c19a67980dc46ec8bf0a01e3b926c
lai2-24 <lai2-24@itismeucci.com>
2025-10-29 13:34:37 +0100

    Creazione delle socket

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,27 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        ServerSocket MyServerSocket = new ServerSocket(3000);
+        Socket G1 = MyServerSocket.accept();
+        System.out.println("WAIT");
+        MyServerSocket.wait();
+        Socket G2 = MyServerSocket.accept();
+
+        BufferedReader G1_in = new BufferedReader(new InputStreamReader(G1.getInputStream()));
+        PrintWriter G1_out = new PrintWriter(G1.getOutputStream(), true);
+
+        BufferedReader G2_in = new BufferedReader(new InputStreamReader(G2.getInputStream()));
+        PrintWriter G2_out = new PrintWriter(G2.getOutputStream(), true);
+
+
+        
     }
 }
